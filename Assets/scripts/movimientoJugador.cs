ef81979256b451efb8dcd16e4efca5665bc5160b
June <yaniradelpilar623@gmail.com>
2025-10-01 21:09:18 +0100

    se termina el tercer video
    se le añade el movimiento al personaje con wasd y rigibody al personaje.

--- Assets/scripts/movimientoJugador.cs
+++ Assets/scripts/movimientoJugador.cs
@@ -8,6 +8,9 @@ public class movimientoJugador : MonoBehaviour
     public float movimientoEjeY;
     public float movimientoEjeZ;
 
+    //si es una variable float, cuando le añadiamos un valor siempre se le pone una f 
+    public float velocidadMovimiento = 1.5f;
+
     void Start()
     {
 
@@ -16,6 +19,15 @@ public class movimientoJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(movimientoEjeX, movimientoEjeX, movimientoEjeZ);
+        //Izquierda y derecha es EjeX A y D
+        //Arriba y abajo EjeZ W y S
+
+        // anotación * Time.deltaTime = se usa para que aunque uno tenga un pc que de tirones o el flipado de la nasa puedan ir a la misma velocidad independientemente al dispositivo que tengan
+
+        //Si se me invirtieran los controles se podria ponmer un - delante del input para reinvertirlos 
+        movimientoEjeX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadMovimiento;
+        movimientoEjeZ = Input.GetAxis("Vertical") * Time.deltaTime * velocidadMovimiento;
+
+        transform.Translate(movimientoEjeX, movimientoEjeY, movimientoEjeZ);
     }
 }
